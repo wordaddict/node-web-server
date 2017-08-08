948279cd295c73d8ff9a388d08aec45fa330f6b8
Adeyinka Micheal <madeyinka6@gmail.com>
2017-08-08 03:54:59 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+var routes = require('./routes')
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log.')
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to our great and amazing page'
+  })
+});
+
+// app.use('/', routes);
+//
+// app.use((req, res, next) => {
+//   var err = new Error('Not found');
+//   err.status = 404;
+//   next(err);
+// })
+//
+// app.use((err, req, res, next) => {
+//   res.status(err.status || 500);
+//   res.send(err);
+// })
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Unable to request page',
+    error2: 'Unable to get page'
+  });
+});
+var port = 8000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`)
+});
